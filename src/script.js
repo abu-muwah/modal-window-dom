cb8bb0148b31ec50bbecd4b23e4d6a484bf58581
ABU_MUWAH <abu.muwah@gmail.com>
2022-09-11 17:08:25 +0430

    Developing the project

--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
-const showModals = document.querySelector('.show-modals').textContent
-console.log(showModals)
+'use strict'
+
+const showModals = document.getElementsByClassName('show-modals')
+const modal = document.querySelector('.modal')
+const closeModal = document.querySelector('.close-modal')
+const overlay = document.querySelector('.overlay')
+
+for (let i = 0; i < showModals.length; i++) 
+    showModals[i].addEventListener('click', () => {
+        console.log('Button Clicked')
+        modal.classList.remove('hidden')
+    } 
+)
+
+closeModal.addEventListener('click', function () {
+    modal.classList.add('hidden')    
+})
 
